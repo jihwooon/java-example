f1aea3414fb9935086af058fd8fe8bd53cc18650
jihwooon <jihwooon@gmail.com>
2024-03-26 01:20:52 +0900

    first init

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+
+    // JUnit 5
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // AssertJ
+    testImplementation ("org.assertj:assertj-core:3.18.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
